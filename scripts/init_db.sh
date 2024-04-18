6d9d433cd2b956c1a6358dde34244f35dd6e84a5
Ukjae Jeong (Jay) <jeongukjae@gmail.com>
2024-04-18 20:17:04 +0900

    add db init scripts

---
+++ scripts/init_db.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -x
+set -eo pipefail
+
+DB_USER=${POSTGRES_USER:=postgres}
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+
+docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    --name zero2prod-postgres \
+    -d postgres \
+    postgres -N 1000
+
+sleep 5
+
+DATABASE_URL=postgresql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
---
+++ scripts/install_tools.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cargo install sqlx-cli --no-default-features --features rustls,postgres
